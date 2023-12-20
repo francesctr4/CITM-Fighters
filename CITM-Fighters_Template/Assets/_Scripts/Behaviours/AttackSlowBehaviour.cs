ca55faa61f4d594d88de9dcd4952904bc66dba5a
Francesc Teruel Rodríguez <francesc.tr03@gmail.com>
2023-12-20 01:25:34 +0100

    Minor changes

--- CITM-Fighters_Template/Assets/_Scripts/Behaviours/AttackSlowBehaviour.cs
+++ CITM-Fighters_Template/Assets/_Scripts/Behaviours/AttackSlowBehaviour.cs
@@ -10,12 +10,6 @@ public class AttackSlowBehaviour : StateMachineBehaviour
         animator.GetComponent<PlayerController>().SetAtacking(true, UpDown.Up);
     }
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
